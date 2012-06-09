271d548e0baa14d5fb2974ee60e39b13d4e89045
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-09 09:53:04 -0700

    Remove system gem regardless of current environment

--- recipes/remove_chef_system_gem.rb
+++ recipes/remove_chef_system_gem.rb
@@ -1,9 +1,6 @@
-# Chef installed via system gem will only be removed
-# if the attributes tell us to AND we are in omnibus
-
 gem_package 'chef' do
-  action :remove
+  action :purge
   only_if do
-    node[:omnibus_updater][:remove_chef_system_gem] && OmnibusChecker.is_omnibus?
+    node[:omnibus_updater][:remove_chef_system_gem]
   end
 end
