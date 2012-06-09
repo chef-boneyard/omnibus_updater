3f9c3c15989cea75590418d91aa990c1ef4949a8
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-09 11:16:38 -0700

    Refactor and set dynamic info up front to make available to script install method

--- recipes/set_remote_path.rb
+++ recipes/set_remote_path.rb
@@ -1,4 +1,6 @@
 # RULES
+
+platform_name = node.platform
 case node.platform_family
 when 'debian'
   if(node.platform == 'ubuntu')
@@ -13,27 +15,6 @@ when 'debian'
   else
     platform_version = node.platform_version.split('.').first
   end
-
-  file_name = "chef-full_#{node[:omnibus_updater][:version]}_"
-  if(node.kernel.machine.include?('64'))
-    file_name << 'amd64'
-  else
-    file_name << 'i386'
-  end
-  file_name << '.deb'
-
-  omnibus_deb = File.join(
-    node[:omnibus_updater][:base_uri],
-    node.platform + '-' +
-    platform_version + '-' +
-    node.kernel.machine,
-    file_name
-  )
-
-  unless(omnibus_deb == node[:omnibus_updater][:full_uri])
-    node.set[:omnibus_updater][:full_uri] = omnibus_deb
-    Chef::Log.info "Omnibus DEB package location: #{omnibus_deb}"
-  end
 when 'fedora', 'rhel'
   platform_version = case node.platform_version.split('.').first
   when '5'
@@ -43,30 +24,48 @@ when 'fedora', 'rhel'
   else
     raise 'Unsupported version'
   end
-
-  omnibus_rpm = File.join(
-    node[:omnibus_updater][:base_uri],
-    'el-' +
-    platform_version + '-' +
-    node.kernel.machine,
-    "chef-full-#{node[:omnibus_updater][:version]}.#{node.kernel.machine}.rpm"
-  )
-
-  unless(omnibus_rpm == node[:omnibus_updater][:full_uri])
-    node.set[:omnibus_updater][:full_uri] = omnibus_rpm
-    Chef::Log.info "Omnibus RPM package location: #{omnibus_rpm}"
-  end
+  platform_name = 'el'
 else
-  omnibus_script = File.join(
-    node[:omnibus_updater][:base_uri],
-    node.platform + '-' +
-    node.platform_version + '-' +
-    node.kernel.machine,
-    "chef-full-#{node[:omnibus_updater][:version]}-#{node.platform_version}-#{node.kernel.machine}.sh"
-  )
+  platform_version = node.platform_version
+end
 
-  unless(omnibus_script == node[:omnibus_updater][:full_uri])
-    node.set[:omnibus_updater][:full_uri] = omnibus_script
-    Chef::Log.info "Omnibus install script location: #{omnibus_script}"
+if(node[:omnibus_updater][:install_via])
+  install_via = node[:omnibus_updater][:install_via]
+else
+  install_via = case node.platform_family
+  when 'debian'
+    'deb'
+  when 'fedora', 'rhel'
+    'rpm'
+  else
+    'script'
   end
+end
+case install_via
+when 'deb'
+  file_name = "chef-full_#{node[:omnibus_updater][:version]}_"
+  if(node.kernel.machine.include?('64'))
+    file_name << 'amd64'
+  else
+    file_name << 'i386'
+  end
+  file_name << '.deb'
+
+when 'rpm'
+  file_name = "chef-full-#{node[:omnibus_updater][:version]}.#{node.kernel.machine}.rpm"
+else
+  file_name = "chef-full-#{node[:omnibus_updater][:version]}-#{platform_name}-#{platform_version}-#{node.kernel.machine}.sh"
+end
+
+remote_omnibus_file = File.join(
+  node[:omnibus_updater][:base_uri],
+  platform_name + '-' +
+  platform_version + '-' +
+  node.kernel.machine,
+  file_name
+)
+
+unless(remote_omnibus_file == node[:omnibus_updater][:full_uri])
+  node.set[:omnibus_updater][:full_uri] = remote_omnibus_file
+  Chef::Log.info "Omnibus remote file location: #{remote_omnibus_file}"
 end
