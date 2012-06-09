729589466a2f91ff474e669108d48cdb6cabdb26
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-09 09:51:24 -0700

    Use generic path builder. Don't backup packages. Glob gives full path

--- recipes/deb_downloader.rb
+++ recipes/deb_downloader.rb
@@ -1,7 +1,9 @@
+include_recipe 'omnibus_updater::set_remote_path'
 
 remote_file "chef omnibus_package_downloader[#{File.basename(node[:omnibus_updater][:full_uri])}]" do
   path File.join(node[:omnibus_updater][:cache_dir], File.basename(node[:omnibus_updater][:full_uri]))
   source node[:omnibus_updater][:full_uri]
+  backup false
   only_if do
     node[:omnibus_updater][:cache_omnibus_installer] &&
     !File.exists?(
@@ -15,9 +17,7 @@ ruby_block "omnibus_updater[remove old debs]" do
     Dir.glob(File.join(node[:omnibus_updater][:cache_dir], 'chef*.deb')).each do |file|
       unless(file.include?(node[:omnibus_updater][:version]))
         Chef::Log.info "Deleting stale omnibus package: #{file}"
-        File.delete(
-          File.join(node[:omnibus_updater][:cache_dir], file)
-        )
+        File.delete(file)
       end
     end
   end
