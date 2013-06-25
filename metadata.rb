34dd612cb5c20635063c2344065c707fc1968b48
Chris Roberts <chrisroberts.code@gmail.com>
2013-06-24 20:07:33 -0700

    Update version for develop v0.2.5

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "chrisroberts.code@gmail.com"
 license          "Apache 2.0"
 description      "Chef omnibus package updater and installer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.4"
+version          "0.2.5"
 
