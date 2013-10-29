c6b6594110bde839527374110a8805b2dab8eadd
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-29 16:51:28 -0700

    Update conditional to use `Gem::Version` instances for version comparison

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 if(node[:omnibus_updater][:disabled])
   Chef::Log.warn 'Omnibus updater disabled via `disabled` attribute'
-elsif node[:platform] == "debian" and node[:platform_version].scan("5")
+elsif(node[:platform] == 'debian' && Gem::Version.new(node[:platform_version]) < Gem::Version.new('6.0.0'))
   Chef::Log.warn 'Omnibus updater does not support Debian 5'
 else
   include_recipe 'omnibus_updater::downloader'
