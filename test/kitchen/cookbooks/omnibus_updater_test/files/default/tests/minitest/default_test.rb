f9ca54f50b1c4dcc8a14d10494a232ff4e42cea9
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-23 10:07:34 -0800

    Update test check on attribute value

--- test/kitchen/cookbooks/omnibus_updater_test/files/default/tests/minitest/default_test.rb
+++ test/kitchen/cookbooks/omnibus_updater_test/files/default/tests/minitest/default_test.rb
@@ -8,7 +8,7 @@ describe_recipe 'omnibus_updater_test::default' do
     assert(node[:omnibus_updater][:full_url], "Failed to set URI for omnibus package")
   end
 
-  it "does not download the package to the node" do
-    file("/opt/#{File.basename(node[:omnibus_updater][:full_url])}").wont_exist
+  it "does downloads the package to the node" do
+    file("/opt/#{File.basename(node[:omnibus_updater][:full_url])}").must_exist
   end
 end
