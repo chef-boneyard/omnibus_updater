76d21846e14b5ceeae237580e7ed19fa7aa1f816
Eric Helgeson <Eric.Helgeson@target.com>
2014-06-13 14:17:34 -0500

    Add optional checksum to the remote_file resource for file integrity.

--- recipes/downloader.rb
+++ recipes/downloader.rb
@@ -20,6 +20,7 @@ if(remote_path)
     path File.join(node[:omnibus_updater][:cache_dir], File.basename(remote_path))
     source remote_path
     backup false
+    checksum node[:omnibus_updater][:checksum] if node[:omnibus_updater][:checksum]
     action :create_if_missing
     only_if do
       unless(version = node[:omnibus_updater][:version])
