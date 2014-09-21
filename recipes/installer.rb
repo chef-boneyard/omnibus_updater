53209fc56ae920fa2f88383e5082f41f4c3058ac
Joshua Kwan <kwan@schrodinger.com>
2014-09-21 16:40:01 -0400

    More fixes for 'exec' mode
    - Do not allow it when chef-zero is being used (chef client local mode)
      If you do, it only re-execs the worker process, which is not the
      process running the chef-zero server.
    - Add argv on as well in case of a runlist override
    - Skip the install notifier, cut out the middleman. If you include the
      ::install recipe, you want to install, and it will always happen after
      the remote_file. This fixes #65, at the cost of making a few more
      assumptions.

--- recipes/installer.rb
+++ recipes/installer.rb
@@ -13,15 +13,20 @@ end
 ruby_block 'omnibus chef killer' do
   block do
     upgrade_behavior = node[:omnibus_updater][:upgrade_behavior]
-    if upgrade_behavior == 'exec' and Chef::Config[:solo]
-      Chef::Log.info 'Cannot use omnibus_updater "exec" upgrade behavior in solo mode -- changing to "kill".'
-      upgrade_behavior = 'kill'
+    if upgrade_behavior == 'exec'
+      if Chef::Config[:solo] or Chef::Config.local_mode
+        Chef::Log.info 'Cannot use omnibus_updater "exec" upgrade behavior in solo/local mode -- changing to "kill".'
+        upgrade_behavior = 'kill'
+      elsif not RbConfig::CONFIG['host_os'].start_with?('linux')
+        Chef::Log.info 'omnibus_updater "exec" upgrade behavior only supported on Linux -- changing to "kill".'
+        upgrade_behavior = 'kill'
+      end
     end
 
     case upgrade_behavior
       when 'exec'
         Chef::Log.info 'Replacing ourselves with the new version of Chef to continue the run.'
-        exec(node[:omnibus_updater][:exec_command])
+        exec(node[:omnibus_updater][:exec_command], *ARGV)
       when 'kill'
         raise 'New version of Chef omnibus installed. Aborting the Chef run, please restart it manually.'
       else
@@ -44,18 +49,10 @@ execute "omnibus_install[#{File.basename(remote_path)}]" do
   else
     raise "Unknown package type encountered for install: #{File.extname(remote_path)}"
   end
-  action :nothing
   if(node[:omnibus_updater][:restart_chef_service])
     notifies :restart, resources(:service => 'chef-client'), :immediately
   end
   notifies :create, resources(:ruby_block => 'omnibus chef killer'), :immediately
-end
-
-ruby_block 'Omnibus Chef install notifier' do
-  block{ true }
-  action :nothing
-  subscribes :create, resources(:remote_file => "omnibus_remote[#{File.basename(remote_path)}]"), :immediately
-  notifies :run, resources(:execute => "omnibus_install[#{File.basename(remote_path)}]"), node[:omnibus_updater][:upgrade_notification]
   only_if { node['chef_packages']['chef']['version'] != node['omnibus_updater']['version'] }
 end
 
