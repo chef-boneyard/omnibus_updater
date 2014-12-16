be8369b9c44e64f3618f19b2147fc696b20af2b5
Tim Smith <tim@cozy.co>
2014-12-15 16:27:35 -0800

    Avoid deleting chef-server packages if using the same cache dir

--- recipes/old_package_cleaner.rb
+++ recipes/old_package_cleaner.rb
@@ -19,7 +19,7 @@
 
 old_pkgs =
   if(::File.exist?(node[:omnibus_updater][:cache_dir]))
-    Dir.glob(File.join(node[:omnibus_updater][:cache_dir], 'chef*')).find_all do |file|
+    Dir.glob(File.join(node[:omnibus_updater][:cache_dir], 'chef_*')).find_all do |file|
       !file.include?(node[:omnibus_updater][:version].to_s) && !file.scan(/\.(rpm|deb)$/).empty?
     end
   else
