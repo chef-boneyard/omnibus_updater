67ba758f371226c1d7c8af2dc1963b9a9e36da2f
Aaron Baer <aaron@hw-ops.com>
2015-03-23 23:47:12 -0700

    test upgrade to latest 11.18 release

--- test/kitchen/cookbooks/omnibus_updater_test/recipes/version_upgrade.rb
+++ test/kitchen/cookbooks/omnibus_updater_test/recipes/version_upgrade.rb
@@ -1,3 +1,3 @@
-node.set[:omnibus_updater][:version] = '11.16'
+node.set[:omnibus_updater][:version] = '11.18'
 node.set[:omnibus_updater][:kill_chef_on_upgrade] = false
 include_recipe "omnibus_updater"
