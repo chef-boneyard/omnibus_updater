6233ebf21fb9a0ab1fdc0f22867e0f6bd2e867eb
Matt Mencel <mr-mencel@wiu.edu>
2016-02-04 14:30:38 -0600

    Don't try to run on Raspbian

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,6 +21,8 @@ if(node[:omnibus_updater][:disabled])
   Chef::Log.warn 'Omnibus updater disabled via `disabled` attribute'
 elsif(node[:platform] == 'debian' && Gem::Version.new(node[:platform_version]) < Gem::Version.new('6.0.0'))
   Chef::Log.warn 'Omnibus updater does not support Debian 5'
+elsif(node[:platform] == 'raspbian')
+  Chef::Log.warn 'Omnibus updater does not support Raspbian'
 else
   include_recipe 'omnibus_updater::downloader'
   include_recipe 'omnibus_updater::installer'
