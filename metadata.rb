704954bc43ea3e73f25d56c66a3a307b9d99cf46
Michael F. Weinberg <michael@luckymike.com>
2016-03-09 10:52:29 -0800

    Bump version to 1.1.0 for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chrisroberts.code@gmail.com"
 license          "Apache 2.0"
 description      "Chef omnibus package updater and installer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.7"
+version          "1.1.0"
 
 %w(redhat centos amazon scientific oracle debian ubuntu mac_os_x solaris windows).each do |plat|
   supports plat
