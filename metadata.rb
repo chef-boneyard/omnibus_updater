13995141ce14df7378f0c1d710779410e3bcebbe
Tim Smith <tsmith@chef.io>
2016-07-20 15:51:03 -0700

    Transfer ownership to Chef
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name             "omnibus_updater"
-maintainer       "Chris Roberts"
-maintainer_email "chrisroberts.code@gmail.com"
-license          "Apache 2.0"
-description      "Chef omnibus package updater and installer"
+name             'omnibus_updater'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache 2.0'
+description      'Chef omnibus package updater and installer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.1"
+version          '1.1.1'
 
-%w(redhat centos amazon scientific oracle debian ubuntu mac_os_x solaris windows).each do |plat|
-  supports plat
+%w(redhat centos amazon scientific oracle debian ubuntu mac_os_x solaris windows).each do |os|
+  supports os
 end
 
 source_url       'https://github.com/hw-cookbooks/omnibus_updater' if respond_to?(:source_url)
