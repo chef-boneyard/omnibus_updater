ba88286b0aa13ef40f614af8373fb1fbf97166af
Tim Smith <tsmith@chef.io>
2016-07-20 15:45:28 -0700

    Add chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -12,3 +12,5 @@ end
 
 source_url       'https://github.com/hw-cookbooks/omnibus_updater' if respond_to?(:source_url)
 issues_url       'https://github.com/hw-cookbooks/omnibus_updater/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11.0' if respond_to?(:chef_version)
